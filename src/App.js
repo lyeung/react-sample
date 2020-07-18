f0d4194c1daa51fbd5951f6043f53f130ed335d1
Lenming Yeung <lenming.yeung@gmail.com>
2020-07-18 22:51:01 +1000

    Use state.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,159 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    sender: '',
+    recipient: '',
+    description: '',
+    senderAddress: '',
+    signature: '',
+    status: 'NEW',
+  });
+
+  const descriptionChanged = evt => {
+    setState({
+      ...state,
+      description: evt.target.value,
+    });
+  };
+
+  const senderChanged = evt => {
+    setState({
+      ...state,
+      sender: evt.target.value,
+    });
+  };
+  const recipientChanged = evt => {
+    setState({
+      ...state,
+      recipient: evt.target.value,
+    });
+  };
+
+  const senderAddressChanged = evt => {
+    setState({
+      ...state,
+      senderAddress: evt.target.value,
+    });
+  };
+
+  const statusChanged = evt => {
+    setState({
+      ...state,
+      status: evt.target.value,
+    });
+  };
+
+  const isShowReady = () => state.status !== 'NEW';
+
+  const signatureChanged = evt => {
+    setState({
+      ...state,
+      signature: evt.target.value,
+    });
+  };
+
+  const onNext = () => {
+    if (state.status === 'NEW') {
+      setState({
+        ...state,
+
+        status: 'READY',
+      });
+    } else if (state.status === 'READY') {
+      setState({
+        ...state,
+        status: 'ENROUTE',
+      });
+    } else if (state.status === 'ENROUTE') {
+      setState({
+        ...state,
+        status: 'COMPLETED',
+      });
+    }
+  };
+
+  const isShowEnroute = () =>
+    state.status !== 'NEW' && state.status !== 'READY';
+
+  const isShowNotHome = () => isShowEnroute() && state.status !== 'COMPLETED';
+
+  const onNotHome = () => {
+    if (state.status === 'ENROUTE') {
+      setState({
+        ...state,
+        description: '',
+        signature: '',
+        status: 'READY',
+      });
+    }
+  };
+
+  const isCompleted = () => state.status === 'COMPLETED';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        Sender:
+        <input
+          disabled={isCompleted()}
+          type="text"
+          value={state.sender}
+          onChange={evt => senderChanged(evt)}
+        />
+      </div>
+      <div>
+        Recipient:
+        <input
+          disabled={isCompleted()}
+          type="text"
+          value={state.recipient}
+          onChange={evt => recipientChanged(evt)}
+        />
+      </div>
+      {isShowReady() && (
+        <div>
+          Sender Address:
+          <input
+            disabled={isCompleted()}
+            type="text"
+            value={state.senderAddress}
+            onChange={evt => senderAddressChanged(evt)}
+          />
+        </div>
+      )}
+
+      {isShowEnroute() && (
+        <div>
+          <div>
+            Description:
+            <input
+              disabled={isCompleted()}
+              type="text"
+              value={state.description}
+              onChange={evt => descriptionChanged(evt)}
+            />
+          </div>
+          <div>
+            Signature:
+            <input
+              disabled={isCompleted()}
+              type="text"
+              value={state.signature}
+              onChange={evt => signatureChanged(evt)}
+            />
+          </div>
+        </div>
+      )}
+      <div>Status: {state.status}</div>
+      <div>
+        <button disabled={isCompleted()} onClick={onNext}>
+          Next
+        </button>
+        {isShowNotHome() && <button onClick={onNotHome}>Not Home</button>}
+      </div>
     </div>
   );
 }
